fb50464b15740709aeeb400fe5125e0ed1b01c39
cankinik <cankinik@gmail.com>
2022-11-17 22:37:50 -0500

    Track ball using HSV and COM

--- src/main.py
+++ src/main.py
@@ -25,8 +25,20 @@ class Camera():
         else:
             print('Camera frame not ready')
             pass
+    def get_com(self, input_frame):
+        hsv_frame = cv.cvtColor(input_frame, cv.COLOR_BGR2HSV)
+        blurred_hsv_frame = cv.blur(hsv_frame, (1, 1))
+        # The HSV threshold values are hard-coded right now. Might want to change that later on
+        hsv_lower_treshold_for_pingpong_ball = np.array([8, 160, 160])
+        hsv_upper_treshold_for_pingpong_ball = np.array([22, 255, 255])
+        masked_frame = cv.inRange(blurred_hsv_frame, hsv_lower_treshold_for_pingpong_ball, hsv_upper_treshold_for_pingpong_ball)
+        moment = cv.moments(masked_frame, bool(0))
+        x = int(moment["m10"] / moment["m00"])
+        y = int(moment["m01"] / moment["m00"])
+        return x, y
 
-class StereoCamera():
+
+class Stereo_Camera():
     def __init__(self, camera_sources, resolution):
         if resolution == 1080:
             resolution_horizontal = 1920
@@ -71,7 +83,9 @@ class StereoCamera():
         temp_left_frame = self.left_camera.get_frame()
         temp_right_frame = self.right_camera.get_frame()
         return cv.remap(temp_left_frame, self.left_map_x, self.left_map_y, cv.INTER_LINEAR), cv.remap(temp_right_frame, self.right_map_x, self.right_map_y, cv.INTER_LINEAR)
-
+        # Return COM of ball from hsv masking and moment com finding
+    def get_coms(self, left_frame, right_frame):
+        return self.left_camera.get_com(left_frame), self.right_camera.get_com(right_frame)
 
 
 def main():
@@ -84,12 +98,27 @@ def main():
     #         break    
     #     frame = camera.get_frame()
 
-    # Stereo camera test
-    stereo_camera = StereoCamera(['resources/CutVideoLeft.mp4', 'resources/CutVideoRight.mp4'], 1080) 
+    # # Stereo camera test
+    # stereo_camera = Stereo_Camera(['resources/CutVideoLeft.mp4', 'resources/CutVideoRight.mp4'], 1080) 
+    # left_frame, right_frame = stereo_camera.get_undistorted_frames()
+    # while left_frame is not None and right_frame is not None:
+    #     cv.imshow('Left Feed', left_frame)
+    #     cv.imshow('Right Feed', right_frame)
+    #     if cv.waitKey(10) & 0xFF == ord('q'):     # Stop Video by pressing 'q'
+    #         break    
+    #     left_frame, right_frame = stereo_camera.get_frames()
+    # cv.destroyAllWindows()
+    # del stereo_camera
+
+    # Ball tracking test
+    stereo_camera = Stereo_Camera(['resources/CutVideoLeft.mp4', 'resources/CutVideoRight.mp4'], 1080) 
     left_frame, right_frame = stereo_camera.get_undistorted_frames()
     while left_frame is not None and right_frame is not None:
-        cv.imshow('Left Feed', left_frame)
-        cv.imshow('Right Feed', right_frame)
+        left_com, right_com = stereo_camera.get_coms(left_frame, right_frame)
+        marked_left_frame = cv.drawMarker(left_frame, left_com, (0, 0, 255), cv.MARKER_CROSS, 50, 5)
+        marked_right_frame = cv.drawMarker(right_frame, right_com, (0, 0, 255), cv.MARKER_CROSS, 50, 5)
+        cv.imshow('Left Feed', marked_left_frame)
+        cv.imshow('Right Feed', marked_right_frame)
         if cv.waitKey(10) & 0xFF == ord('q'):     # Stop Video by pressing 'q'
             break    
         left_frame, right_frame = stereo_camera.get_frames()
